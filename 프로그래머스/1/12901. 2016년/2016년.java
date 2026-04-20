0296943e3051475372192eddc3046ba8eeee1de7
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-20 09:54:52 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30};
+        int answer = 0;
+        
+        for (int i = 0; i < a - 1; i++) {
+            answer += month[i];
+        }
+        answer += b - 1;
+        
+        return day[answer % 7];
+    }
+}
