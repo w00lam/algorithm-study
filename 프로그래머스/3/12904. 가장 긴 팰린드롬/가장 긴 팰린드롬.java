51f3683b4cd45327de37c96e8b444551c62bb410
woolam <98564214+w00lam@users.noreply.github.com>
2026-07-06 09:52:10 +0900

    Refactor palindrome length calculation logic

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
 
         for (int center = 0; center < s.length(); center++) {
-            int oddLength = expand(s, center, center);       // 홀수 길이
-            int evenLength = expand(s, center, center + 1);  // 짝수 길이
+            int oddLength = expand(s, center, center);
+            int evenLength = expand(s, center, center + 1);
 
             answer = Math.max(answer, oddLength);
             answer = Math.max(answer, evenLength);
